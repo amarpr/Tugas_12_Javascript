7230563dc2b21f722d7b2d31e7e7915f97375a74
Muhamar <amararrasyid@gmail.com>
2020-05-12 17:39:36 +0700

    Tugas 12 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,12 @@
+function namaBuah() {
+    var buah = ['Pisang', 'Jeruk', 'Apel', 'Mangga']
+    console.log(buah)
+    buah.pop()
+    console.log(buah)
+    buah.shift()
+
+    return buah;
+
+}
+
+console.log(namaBuah())
